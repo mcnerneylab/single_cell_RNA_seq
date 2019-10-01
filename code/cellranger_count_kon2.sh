c5403dae36753e94e28472b31dd0074fbfc58009
liuweihanty <43444815+liuweihanty@users.noreply.github.com>
2019-10-01 17:15:33 -0500

    Update cellranger_count_kon2.sh

--- code/cellranger_count_kon2.sh
+++ code/cellranger_count_kon2.sh
@@ -24,8 +24,7 @@ module load gcc/6.2.0
 module load cellranger/3.0.2
 
 cellranger count --id=kon2 --transcriptome=/gpfs/data/mcnerney-lab/liuweihan/cellranger_ref_genomes/mm10genome_chrM \
-
---fastqs=/gpfs/data/mcnerney-lab/liuweihan/scRNA/raw_data_ln1/H77TNBBXY_5/KON-2,/gpfs/data/mcnerney-lab/liuweihan/scRNA/raw_data_ln2/KON-2  --sample=KON-1 --expect-cells=1000
+--fastqs=/gpfs/data/mcnerney-lab/liuweihan/scRNA/raw_data_ln1/H77TNBBXY_5/KON-2,/gpfs/data/mcnerney-lab/liuweihan/scRNA/raw_data_ln2/KON-2  --sample=KON-2 --expect-cells=1000
 
 out=/gpfs/data/mcnerney-lab/liuweihan/scRNA/combined_ln1_2/count_out
 
